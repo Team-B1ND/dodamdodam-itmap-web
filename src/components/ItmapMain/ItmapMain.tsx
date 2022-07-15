3e5c242226e1976e8ad1a28e8218106fdbc50a87
nrbns357 <gusgh357@gmail.com>
2022-07-15 23:26:10 +0900

    ⚡:: map main 폼 구조 변경

--- src/components/ItmapMain/ItmapMain.tsx
+++ src/components/ItmapMain/ItmapMain.tsx
@@ -1,11 +1,10 @@
-import * as S from "./ItmapMain.style";
-// import useItMapContainer from "../../hooks/useItMapContainer/useItMapContainer";
-import styled from "styled-components";
-import useData from "hooks/useData";
-import { Map, ZoomControl, MapTypeControl, MapMarker } from "react-kakao-maps-sdk";
-import { useEffect, useState } from "react";
+import { Map, ZoomControl, MapTypeControl } from "react-kakao-maps-sdk";
+import { useState } from "react";
 import useMarker from "hooks/useMarker";
 import MapMarkerList from "components/MapMarkerList";
+import NavContainer from "components/ItmapNav/index";
+import CompanyRegistration from "components/CompanyRegistration/index";
+
 
 
 declare global {
@@ -17,36 +16,11 @@ declare global {
 const { kakao } = window;
 
 const Itmap = () => {
-
-  // const { MapSingleton } = useItMapContainer();
-  const { testData } = useData();
-
-
-
-  const geocoder = new kakao.maps.services.Geocoder();
   const [map, setMap] = useState<kakao.maps.Map | undefined>();
-
-
-
-  // useEffect(() => {
-  //   MapSingleton.getInstance().initMap({ testData });
-  // }, [MapSingleton, testData]);
-
-
-  const [position, setPosition] = useState({
-    lat: 36.5,
-    lng: 127.5
-  });
-
-  interface props {
-    result: any;
-    status: number;
-  }
-
   const markers = useMarker(map);
 
   return (
-    <MapWrapper>
+    <>
       <Map
         center={
           {
@@ -56,32 +30,22 @@ const Itmap = () => {
         }
         isPanto={true}
         style={{
-          // 지도의 크기
-          width: "100%",
-          height: "100%",
+          position: 'fixed',
+          width: 'calc(100vw - 384px)',
+          height: '100%',
+          right: '0px',
         }}
-        level={12}
         onCreate={setMap}
+        level={12}
       >
-        <MapMarkerList markers={markers}></MapMarkerList>
-        <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+        <NavContainer />
+        <MapMarkerList markers={markers} />
         <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
       </Map>
-
-    </MapWrapper >
-
-
+      <CompanyRegistration />
+    </>
   );
 };
-
-const MapWrapper = styled.div`
-  width: 85%;
-  height: 100%;
-
-  @media screen and (max-width: 500px) {
-    height: 60vh;
-  }
-`;
 
 export default Itmap;
 
