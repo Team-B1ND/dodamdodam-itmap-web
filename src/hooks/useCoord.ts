827e7e6c24c28d2e71999689f75b33cd09c27bef
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:49:53 +0900

    ⚡:: 주소로 marker를 표시할 좌표로 변화시켜주는 hooks 작성

---
+++ src/hooks/useCoord.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useMap } from "react-kakao-maps-sdk";
+import useData from "./useData";
+
+interface coordsProps {
+  lat: number;
+  lng: number;
+}
+
+const useCoord = (map: kakao.maps.Map | undefined, location: string) => {
+  const [coord, setCoord] = useState<coordsProps>();
+
+  useEffect(() => {
+    if (!map) return;
+
+    const gc = new kakao.maps.services.Geocoder();
+
+    gc.addressSearch(location, (result, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setCoord({
+          lat: Number(result[0].y),
+          lng: Number(result[0].x),
+        });
+      }
+    });
+  }, [location, map]);
+
+  return [coord?.lat, coord?.lng];
+};
+
+export default useCoord;
