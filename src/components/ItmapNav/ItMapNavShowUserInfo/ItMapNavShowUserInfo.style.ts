6bb25033e2659a2965dc9c5c5312d5c047dcfa46
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:45:57 +0900

    Update ItMapNavShowUserInfo.style.ts

--- src/components/ItmapNav/ItMapNavShowUserInfo/ItMapNavShowUserInfo.style.ts
+++ src/components/ItmapNav/ItMapNavShowUserInfo/ItMapNavShowUserInfo.style.ts
@@ -1,34 +1,70 @@
 import { palette } from "style/palette";
 import styled from "styled-components";
 
-export const ShowUserListContainer = styled.button`
-  width: 90%;
-  height: 150px;
-  margin-top: 25px;
-  padding: 15px;
-  border-radius: 5px;
-  border: none;
+export const ShowUserListContainer = styled.div`
+  position: absolute;
+  left: 384px;
+  top: 0px;
+  min-width: 270px;
+  height: 100%;
+  padding: 10px;
   background-color: #fff;
-  text-align: left;
-  transition: 0.3s;
-  box-shadow: 0px 0px 20px 5px ${palette.boxShadowColor};
-  cursor: pointer;
-  font-size: medium;
-  &:hover {
-    background-color: #f1f1f1;
-  }
-  &:focus {
-    background-color: #d1d1d1;
-  }
+  flex-direction: column;
+  box-shadow: ${palette.boxShadowColor};
 `;
 
-export const UserCompanyNameContainer = styled.div`
+export const CompanyInfoListContainerWrapper = styled.div`
+  width: 100%;
+  background-color: ${palette.main};
+  color: ${palette.gray[100]};
+  padding: 7px;
+  border-radius: 5px;
+  margin-top: 100px;
+`;
+
+export const CompanyInfoListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const UserNameContainer = styled.div`
   font-size: 1rem;
-  margin-bottom: 4px;
   font-weight: bold;
 `;
 
-export const UserCompanyLocationContainer = styled.div`
-  font-size: 12px;
-  margin-bottom: 4px;
+export const UserDevPosition = styled.div`
+  text-align: center;
+  padding: 1px 3px;
+  background-color: ${palette.lightBlue[200]};
+  border-radius: 5px;
+`;
+
+export const CloseBtnContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  font-size: 20px;
+  cursor: pointer;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  transform: translate(260px, 70px);
+  box-shadow: ${palette.boxShadowColor};
+  &:hover {
+    color: ${palette.main};
+  }
+`;
+
+export const UserExplanation = styled.div`
+  margin-top: 4px;
+  font-size: 0.7rem;
+`;
+
+export const Generation = styled.div`
+  margin-top: 4px;
+  font-size: 0.7rem;
+  padding: 0 3px;
 `;
