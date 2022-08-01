47a9b13c81884d4aa9392bd61ee3a2b8903f1992
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:42:11 +0900

    ⚡:: 회사 등록 폼 구현

--- src/components/CompanyRegistration/CompanyRegistration.style.ts
+++ src/components/CompanyRegistration/CompanyRegistration.style.ts
@@ -1,5 +1,5 @@
 import { palette } from "style/palette";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 export const CompanyRegistrationContanier = styled.div`
   width: 240px;
@@ -11,6 +11,7 @@ export const CompanyRegistrationContanier = styled.div`
   background-color: white;
   display: flex;
   justify-content: space-between;
+  box-shadow: 0 3px 1px 0 rgb(0 0 0 / 15%);
 `;
 
 export const CompanyRegistrationMent = styled.div`
@@ -30,7 +31,6 @@ export const CompanyRegistrationBtn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: ${palette.boxShadowColor};
   background-color: ${palette.main};
   transition: 0.4s;
   cursor: pointer;
@@ -43,6 +43,7 @@ export const CompanyRegistrationBtn = styled.div`
 export const CompanyRegistrationModalBackground = styled.div`
   width: 100%;
   height: 100%;
+  position: absolute;
   background-color: rgba(0, 0, 0, 0.65);
   z-index: 10;
 `;
@@ -52,16 +53,19 @@ export const CompanyRegistrationModal = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 320px;
-  height: 400px;
+  width: 380px;
   background-color: white;
-  padding: 24px;
+  padding: 25px;
   border-radius: 5px;
   #modalCloseBtn {
     position: absolute;
     top: 7px;
     right: 7px;
+    font-size: 1.5rem;
     cursor: pointer;
+  }
+  #modalCloseBtn:hover {
+    color: ${palette.main};
   }
 `;
 
@@ -78,14 +82,85 @@ export const NameInput = styled.input`
   }
 `;
 
-export const CompanyAddressInput = styled.input`
+export const CompanyAddressContainer = styled.div`
   width: 100%;
   height: 30px;
   margin: 20px 0;
-  padding-left: 10px;
-  outline: none;
-  border: 1px solid ${palette.gray[300]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+export const UserIntroduceMent = styled.input`
+  width: 100%;
+  height: 30px;
+  outline: none;
+  padding-left: 10px;
+  border: 1px solid ${palette.gray[300]};
+  &:focus {
+    border: 1px solid ${palette.gray[500]};
+  }
+`;
+
+export const CompanyNameInput = styled.input`
+  width: 100%;
+  height: 30px;
+  outline: none;
+  margin-top: 20px;
+  padding-left: 10px;
+  border: 1px solid ${palette.gray[300]};
+  &:focus {
+    border: 1px solid ${palette.gray[500]};
+  }
+`;
+
+export const CompanyAddressBox = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${palette.gray[300]};
+  &:focus {
+    border: 1px solid ${palette.gray[500]};
+  }
+`;
+
+export const CompanyAddressSearch = styled.button`
+  width: 30%;
+  height: 100%;
+  outline: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+  background-color: ${palette.main};
+`;
+
+export const DaumPostSerchContanier = styled.div`
+  width: calc(380px - 50px);
+`;
+
+export const CompanyRegistrationSubmitBtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const CompanyRegistrationSubmitBtn = styled.button`
+  border: none;
+  color: white;
+  padding: 12px;
+  border-radius: 4px;
+  background-color: ${palette.main};
+`;
+
+export const DevPosition = styled.input`
+  width: 100%;
+  height: 30px;
+  outline: none;
+  margin-top: 20px;
+  padding-left: 10px;
+  border: 1px solid ${palette.gray[300]};
   &:focus {
     border: 1px solid ${palette.gray[500]};
   }
--- src/components/CompanyRegistration/CompanyRegistration.tsx
+++ src/components/CompanyRegistration/CompanyRegistration.tsx
@@ -1,22 +1,74 @@
 import * as S from "./CompanyRegistration.style";
 import { FiPlus } from "react-icons/fi";
-import { useState } from "react";
-import { AiFillCloseCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { FiX } from "react-icons/fi";
+import DaumPostcode from "react-daum-postcode";
+import { UserNameContainer } from "components/ItmapNav/ItMapNavShowUserInfo/ItMapNavShowUserInfo.style";
+
+interface CompanyRegistrationDataType {
+  userName: string;
+  IntroduceMent: string;
+  devPosition: string;
+}
 
 
 const CompanyRegistration = () => {
 
-  const [isCompanyRegistrationToggle, setIsCompanyRegistrationContanier] = useState<boolean>(false);
+  const [isCompanyRegistrationToggleOpen, setIsCompanyRegistrationToggleOpen] = useState<boolean>(false);
+  const [isDaumPostOpen, setIsDaumPostOpen] = useState<boolean>(false);
+  const [daumSearchResultValue, setDaumSearchResultValue] = useState<string>("");
+  const [companyRegistrationData, setCompanyRegistrationData] = useState<CompanyRegistrationDataType>({
+    userName: "",
+    IntroduceMent: "",
+    devPosition: "",
+  })
+
+
 
   return (
     <>
-
-      {isCompanyRegistrationToggle &&
+      {isCompanyRegistrationToggleOpen &&
         <S.CompanyRegistrationModalBackground>
           <S.CompanyRegistrationModal>
-            <AiFillCloseCircle id="modalCloseBtn" onClick={() => setIsCompanyRegistrationContanier(false)} />
-            <S.NameInput type="text" placeholder="이름" />
-            <S.CompanyAddressInput type="text" placeholder="회사 주소" />
+            <FiX id="modalCloseBtn" onClick={() => {
+              setIsCompanyRegistrationToggleOpen(false);
+              setIsDaumPostOpen(false);
+            }} />
+            <S.NameInput type="text" placeholder="사용자 이름" onChange={(e) => setCompanyRegistrationData({ ...companyRegistrationData, userName: e.target.value })} />
+
+            <S.UserIntroduceMent type="text" placeholder="소개 말 (ex: 항상 노력하는 개발자입니다)" onChange={(e) => setCompanyRegistrationData({ ...companyRegistrationData, IntroduceMent: e.target.value })} />
+
+            <S.CompanyNameInput type="text" placeholder="회사 이름" />
+
+            <S.DevPosition type="text" placeholder="자신의 분야" onChange={(e) => setCompanyRegistrationData({ ...companyRegistrationData, devPosition: e.target.value })} />
+
+
+            <S.CompanyAddressContainer>
+              {/* 검색 결과가 div의 크기를 넘었을 때 처리해야함 */}
+              <S.CompanyAddressBox placeholder="회사 주소" >{daumSearchResultValue ? daumSearchResultValue : "현재 검색 결과가 없습니다"}</S.CompanyAddressBox>
+              <S.CompanyAddressSearch onClick={() => setIsDaumPostOpen(!isDaumPostOpen)} >{isDaumPostOpen ? "검색 창 닫기" : "주소 찾기"}</S.CompanyAddressSearch>
+            </S.CompanyAddressContainer>
+            <S.DaumPostSerchContanier>
+
+              {
+                isDaumPostOpen &&
+                <DaumPostcode
+                  onComplete={(value) => {
+                    setDaumSearchResultValue(value.address);
+                    setIsDaumPostOpen(false);
+                  }}
+                  autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
+                />
+              }
+            </S.DaumPostSerchContanier>
+
+            <S.CompanyRegistrationSubmitBtnContainer>
+
+              <S.CompanyRegistrationSubmitBtn>
+                등록하기
+              </S.CompanyRegistrationSubmitBtn>
+            </S.CompanyRegistrationSubmitBtnContainer>
+
           </S.CompanyRegistrationModal>
         </S.CompanyRegistrationModalBackground>
       }
@@ -28,7 +80,7 @@ const CompanyRegistration = () => {
         </S.CompanyRegistrationMent>
 
         <S.CompanyRegistrationBtn onClick={() => {
-          setIsCompanyRegistrationContanier(true);
+          setIsCompanyRegistrationToggleOpen(true);
         }}>
           <FiPlus />
         </S.CompanyRegistrationBtn>
