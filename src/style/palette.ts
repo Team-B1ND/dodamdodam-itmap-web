ee78006ac71d1e6c272f0d223783bee3dbccc9e2
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:56:52 +0900

    ⚡:: global 스타일 적용, ⚡:: palette 변경

--- src/style/palette.ts
+++ src/style/palette.ts
@@ -152,8 +152,8 @@ export const palette = {
   purple,
   indigo,
   random,
-  main: "#0067bc",
-  white: "#ffffff",
-  black: "#000000",
+  main: "#0067BC",
+  borderColor: "#c4c4c4",
   modalBackground: "rgba(30, 30, 30, 0.6)",
+  boxShadowColor: " 5px 0px 7px 0 rgb(0 0 0 / 15%)",
 };
