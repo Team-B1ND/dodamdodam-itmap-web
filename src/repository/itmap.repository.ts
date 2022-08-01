8ec78230a4a1703755861d9d2526ba8c50c25e20
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:54:33 +0900

    ⚡:: 서버와 통신 로직 작성 (아직 서버가 완성 되지 않아서 작성만 해놓았음)

--- src/repository/itmap.repository.ts
+++ src/repository/itmap.repository.ts
@@ -2,8 +2,8 @@ import customAxios from "../lib/axios/customAxios";
 import { ItMapResponse } from "../types/itmap/itmap.type";
 
 class itMapRepository {
-  public async itMap(): Promise<ItMapResponse> {
-    const { data } = await customAxios.get<ItMapResponse>("/itmap/join");
+  public async getItMapData(): Promise<ItMapResponse> {
+    const { data } = await customAxios.get<ItMapResponse>("/itmap");
     return data;
   }
 }
