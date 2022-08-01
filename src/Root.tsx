3d4e15de29e2b03ca679aa27226a04a9077cb6db
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:54:55 +0900

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,20 +2,24 @@ import React, { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./components/App";
+import store from "store/reducers";
+
 
 const Root = () => {
 
-    const queryClient = new QueryClient();
+    // const queryClient = new QueryClient();
 
     return (
-        // <Provider>
-        <StrictMode>
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>
-        </StrictMode>
-        // </Provider>
+        <Provider store={store}>
+            {/* <StrictMode> 리렌더링이 되게 한다 */}
+            {/* <QueryClientProvider client={queryClient}> */}
+            <App />
+            {/* </QueryClientProvider> */}
+            {/* </StrictMode> */}
+        </Provider>
     );
 };
 
 export default Root;
+
+
