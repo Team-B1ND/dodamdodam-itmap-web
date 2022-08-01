0b9820880e0b54b9dc9e151336882031ed83fa32
nrbns357 <gusgh357@gmail.com>
2022-08-01 14:50:11 +0900

    ⚡:: testData

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,96 +1,82 @@
-import { useState } from "react";
-
 const useData = () => {
   const testData = [
     {
       id: "1",
       name: "정우재",
-      generation: "4",
+      generation: "1",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "나는 크다",
+      explanation: "안녕하세ㅐ요 저는 좀 모자랍니다",
       companyName: "(주)북한 인민군",
       companyLocation: "경기도",
-      position: { lat: 36.5, lng: 127 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "2",
       name: "박병관",
-      generation: "4",
+      generation: "3",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "나는 크다",
+      explanation: "저는 헬창이 되서 찌찌를 키울거에요!",
       companyName: "(주)태평양 하와이안",
       companyLocation: "대구",
-      position: { lat: 36.7, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "3",
       name: "임동현",
-      generation: "4",
+      generation: "5",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "나는 크다",
+      explanation: "커플은 우우우",
       companyName: "원티드랩",
-      companyLocation: "광주",
-      position: { lat: 36.6, lng: 127.5 },
+      companyLocation: "안양",
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "4",
       name: "김상은",
-      generation: "4",
+      generation: "6",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "나는 크다",
+      explanation: "나는 크다 넵 저는 이 말대로 큽니다",
       companyName: "FLO",
       companyLocation: "서울",
-      position: { lat: 36.5, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "5",
       name: "기준",
-      generation: "4",
+      generation: "7",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "나는 크다",
+      explanation: "세상의 기준이 되는 남자 김기준 입니다",
       companyName: "(주)페르소나",
       companyLocation: "울산",
-      position: { lat: 36.6, lng: 127 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "6",
       name: "금현호",
-      generation: "4",
+      generation: "6",
       profileImg:
         "https://user-images.githubusercontent.com/48943501/127284705-696db588-a867-48b6-aeba-8c2cd28d7f2f.jpg",
       tagImg:
         "https://user-images.githubusercontent.com/48943501/127284435-aeeb1458-15ae-4dbf-bcff-7cbd28556a0c.jpg",
-      explanation: "i'm big",
+      explanation: "안녕하세요 저는 박사모 입니다",
       companyName: "Google",
-      companyLocation: "창원",
-      position: { lat: 35.7, lng: 126.4 },
+      companyLocation: "강남",
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "7",
@@ -103,9 +89,7 @@ const useData = () => {
       explanation: "나는 크다",
       companyName: "(주)북한 인민군",
       companyLocation: "경기도 광주",
-      position: { lat: 35.8, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "8",
@@ -118,9 +102,7 @@ const useData = () => {
       explanation: "나는 크다",
       companyName: "(주)태평양 하와이안",
       companyLocation: "제주도",
-      position: { lat: 35.7, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "9",
@@ -133,9 +115,7 @@ const useData = () => {
       explanation: "나는 크다",
       companyName: "원티드랩",
       companyLocation: "포항",
-      position: { lat: 35.6, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "10",
@@ -148,9 +128,7 @@ const useData = () => {
       explanation: "나는 크다",
       companyName: "FLO",
       companyLocation: "인천",
-      position: { lat: 35.5, lng: 127.5 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "11",
@@ -163,9 +141,7 @@ const useData = () => {
       explanation: "나는 크다",
       companyName: "(주)페르소나",
       companyLocation: "파주",
-      position: { lat: 35.6, lng: 127 },
       devPosition: "FE",
-      isOpen: false,
     },
     {
       id: "12",
@@ -178,9 +154,7 @@ const useData = () => {
       explanation: "i'm big",
       companyName: "Google",
       companyLocation: "부산",
-      position: { lat: 35.7, lng: 126.4 },
       devPosition: "FE",
-      isOpen: false,
     },
   ];
 
