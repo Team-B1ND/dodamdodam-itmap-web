5fe0e3afb7d2bfe4acf5c9084187cbd71133ae3b
nrbns357 <gusgh357@gmail.com>
2022-08-23 20:05:58 +0900

    ⚡:: 주소로 좌표 뽑아내는 기능 구현

---
+++ src/hooks/companyCoords/useCoord.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+interface coordsProps {
+  lat: number;
+  lng: number;
+}
+
+const useCoord = (map: kakao.maps.Map | undefined, location: string) => {
+  const [coord, setCoord] = useState<coordsProps>();
+
+  useEffect(() => {
+    if (!map) return;
+
+    const gc = new kakao.maps.services.Geocoder();
+    gc.addressSearch(location, (result, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setCoord({
+          lat: Number(result[0].y),
+          lng: Number(result[0].x),
+        });
+      }
+    });
+  }, [location, map]);
+
+  return [coord?.lat, coord?.lng];
+};
+
+export default useCoord;
