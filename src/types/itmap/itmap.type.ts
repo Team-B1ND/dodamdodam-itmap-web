ef516cdeb918c12b4113d431dba9d486abeb1ef1
nrbns357 <gusgh357@gmail.com>
2022-09-01 11:49:38 +0900

    ⚡:: type 재정의

--- src/types/itmap/itmap.type.ts
+++ src/types/itmap/itmap.type.ts
@@ -1,23 +1,7 @@
-import { Response } from "../util/response.type";
-
 export type ItMapLatLng = {
   lat: number;
   lng: number;
 };
-
-export type UserInfo = {
-  address: string;
-  id: string;
-  name: string;
-  position: {
-    lat: number;
-    lng: number;
-  };
-};
-
-export interface UserInfoResponse extends Response {
-  data: UserInfo[];
-}
 
 export type ItMapData = {
   companyPlaceId: string;
