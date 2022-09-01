de8da63ab667b1aefcad293b156be970dae26156
nrbns357 <gusgh357@gmail.com>
2022-09-01 19:46:00 +0900

    ⚡:: 글로벌 폰트 적용

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,14 +2,17 @@ import React, { StrictMode } from "react";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import store from "store/reducers";
-
+import GlobalStyle from "style/global";
 
 const Root = () => {
 
 
     return (
         <Provider store={store}>
+            {/* <StrictMode> */}
+            <GlobalStyle />
             <App />
+            {/* </StrictMode> */}
         </Provider>
     );
 };
