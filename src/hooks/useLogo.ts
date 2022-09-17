1e5715d70dfa25d4514723efc3065184e040ad07
nrbns357 <gusgh357@gmail.com>
2022-09-17 20:06:46 +0900

    ⚡:: 회사 로고 hooks로 관리

---
+++ src/hooks/useLogo.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import GOOGLE from "assets/companyLogoImg/GOOGLE_LOGO.png";
+import BANK_SAL from "assets/companyLogoImg/bank_sal.png";
+import MIDAS from "assets/companyLogoImg/midas_logo.png";
+// import BASIC from "assets/companyLogoImg/basics.png";
+
+const useLogo = () => {
+  const [logoLists, setLogoLists] = useState([
+    "https://static.toss.im/tds/favicon/favicon-196x196.png",
+    GOOGLE,
+    BANK_SAL,
+    null,
+    MIDAS,
+  ]);
+  return { logoLists };
+};
+
+export default useLogo;
